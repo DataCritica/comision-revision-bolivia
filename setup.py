0d00f7c90c399c0e06b32bf6ca9e6a2a67b52e19
Fer Aguirre <kay_op@protonmail.com>
2022-07-21 15:12:02 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Comision Revision Bolivia',
+    version='0.1.0',
+    author='Fer Aguirre',
+    description='Análisis de datos para revisar los resultados del trabajo de la Comisión de Revisión de Casos de Violación y Feminicidio en Bolivia',
+    python_requires='>=3',
+    license='MIT License',
+    packages=find_packages(),
+)
