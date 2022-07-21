58e178a283968024fc0571f1dc60e37ba73fa952
Fer Aguirre <kay_op@protonmail.com>
2022-07-21 17:04:34 -0500

    Add relative paths

---
+++ comision_revision_bolivia/data/load.py
@@ -0,0 +1,7 @@
+# Inputs
+data_raw = path.data_raw_dir("feminicidios_bolivia.xlsx")
+
+# Outputs
+data_processed = path.data_processed_dir()
+tables =  path.outputs_tables_dir()
+figures = path.outputs_figures_dir()
